93a56cb784ab5ceb2f698e8dc7029cf027187a2c
Jaki Widby <jaki.widby@gmail.com>
2020-08-13 11:33:06 -0700

    add additional quiz questions, add if statement to js

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  // $("introButton").submit(function(event) {
+  //   event.preventDefault();
+  //   const name = $("input#name").val();
+  //   if (name) {
+  //     $("#quiz").show();
+  //   } else {
+  //     alert("Please enter your name!");
+  //   }
+  // });
+
+  $("form").submit(function() { 
+  
+    event.preventDefault();
+    const height = $("input:radio[name=height]:checked").val();
+
+    const date = $("input:radio[name=date]:checked").val();
+
+    const food = $("input:radio[name=food]:checked").val();
+
+    
+
+    if (height === "bean" && food === "tuna" || height === "bean" && food ==="licorice" || height === "bean" && food ==="brocolli") {
+      $("#quizResultGoblin").hide();
+      $("#quizResultSponge").hide();
+      $("#quizResultBean").show();
+
+    } else if (height === "spongeBob" && food === "brocolli" || height === "spongeBob" && food ==="tuna" || height === "spongeBob" && food === "licorice") {
+      $("#quizResultBean").hide();
+      $("#quizResultGoblin").hide();
+      $("#quizResultSponge").show();
+
+    } else {
+      $("#quizResultBean").hide();
+      $("quizResultSponge").hide();
+      $("#quizResultGoblin").show();
+    }
+  });
+    
+});
